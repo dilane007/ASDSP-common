9b0f9fc94984f0efbf5b12736c3b5863699640b8
dilane007 <danielfrancismartin@gmail.com>
2015-08-03 16:31:43 -0700

    Revert "math"
    This reverts commit c13174bad68cf4765c0fc745cfc1610be2c660af.

--- mathutil/cart2pold2.m
+++
@@ -1,7 +0,0 @@
-%[TH,R] = CART2POLD(X,Y) TH is returned in degrees. 
-% transforms corresponding elements of data stored in Cartesian coordinates X,Y to polar coordinates (angle TH and radius R).  
-% The arrays X and Y must be the same size (or either can be scalar). TH is returned in degrees. 
-function [th,r,z] = cart2pold(x,y,z)
-  th = 180/pi*atan2(y,x);
-  r = sqrt(x.^2+y.^2);
-end
